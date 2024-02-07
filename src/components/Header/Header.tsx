65f4c737ba3e1b7a324e7ce2fce307133fef7477
kelocoes <kevin.rodriguez109@gmail.com>
2024-02-07 14:42:07 -0500

    Fix sonar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -22,10 +22,10 @@ export default function Header({ setSection }: HeaderProps): JSX.Element {
                 {matches ?
                     <ul className="menu menu-vertical sm:menu-horizontal rounded-box bg-white text-slate-600 lg:w-max items-center shadow-lg py-1">
                         <img src={logo} alt="Logo Psic Gofran" className="w-auto h-14 p-0 mr-2" />
-                        <li><a className="font-body font-semibold text-lg" onClick={() => choose(0)} role="button">Inicio</a></li>
-                        <li><a className="font-body font-semibold text-lg" onClick={() => choose(1)} role="button">Sobre mi</a></li>
-                        <li><a className="font-body font-semibold text-lg truncate" onClick={() => choose(2)} role="button">Tratamientos / Servicios</a></li>
-                        <li><a className="font-body font-semibold text-lg" onClick={() => choose(3)} role="button">Contáctame</a></li>
+                        <li><button className="font-body font-semibold text-lg" onClick={() => choose(0)} type="button">Inicio</button></li>
+                        <li><button className="font-body font-semibold text-lg" onClick={() => choose(1)} type="button">Sobre mi</button></li>
+                        <li><button className="font-body font-semibold text-lg truncate" onClick={() => choose(2)} type="button">Tratamientos / Servicios</button></li>
+                        <li><button className="font-body font-semibold text-lg" onClick={() => choose(3)} type="button">Contáctame</button></li>
                     </ul>
                     :
                     <ul className="menu rounded-box bg-white text-black lg:w-max items-center shadow-lg py-1">
